99e8f6d201403e945b142c1e64463690fc004f81
Ben Hoberg <benjhoberg@gmail.com>
2023-02-23 14:04:55 -0600

    challenge completed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,84 @@
 ﻿using System;
 
-namespace EnthusiasticMoose
+Main();
+
+void Main()
 {
-    class Program
+    Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine();
+
+    // Let the moose speak!
+    MooseSays("H I, I'M  E N T H U S I A S T I C !");
+    MooseSays("I really am enthusiastic");
+
+    Question("yes or no?", "yes", "no");
+    Question("maybe or definitely?", "absolutely", "I'm not sure");
+    Question("how many?", "a few", "too many");
+    Question("did these questions confuse you?", "wait, what?", "bumblebee tuna");
+}
+void MooseSays(string message)
+{
+    Console.WriteLine($@"
+                                      _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
+
+void Question(string query, string yes, string no)
+{
+    bool isTrue = MooseAsks(query);
+    if (isTrue)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        MooseSays(yes);
+    }
+    else
+    {
+        MooseSays(no);
     }
 }
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
